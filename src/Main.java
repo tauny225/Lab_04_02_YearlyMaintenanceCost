a35ada02c1db2a4b0a4bb09707b1b3e741677613
DJEDOU KODJO <tauny225@gmail.com>
2022-06-24 22:24:29 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double springCost = 1000;
+        double summerCost = 1550;
+        double fallCost = 2000;
+        double winterCost = 2550.50;
+        double totalCosts = 0;
+
+        totalCosts = springCost + summerCost + fallCost + winterCost;
+        System.out.println("The total yearly home maintenance cost is:" + totalCosts);
+	// write your code here
+    }
+}
